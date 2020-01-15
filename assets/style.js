bc6de40f34ca5a6e45c4859d523da5dc73a17db9
Lrt95 <antony.correia@gmail.com>
2020-01-15 12:16:01 +0100

    declenchement et afficahge chrono

--- assets/style.js
+++ assets/style.js
@@ -1,10 +1,8 @@
-/*var b= document.body;
-var new_p = document.createElement('p');
-new_p.textContent = "coizecozico";
-b.prepend(new_p);*/
-
-select = document.getElementById('select')
-
+select = document.getElementById('select');
+date = 0;
+date2 = 0;
+dateStop = 0;
+chrono = "00"
 
 
 
@@ -13,7 +11,7 @@ function getValue(){
 
 
 function newGame(){
-	console.log(select.value)
+	console.log(select.value);
 	if (select.value==9) {
 		nbBombes=10
 	}
@@ -27,5 +25,61 @@ function newGame(){
 		nbBombes=250
 	}
 	console.log(nbBombes);
-}	
+
+	startChrono()
+}
+
+
+
+
+function startChrono()
+{
+	date1 = Date.now();
+	date2 = setInterval(function()
+	{
+		if (date2 == 0)
+			date = Date.now() - date1;
+		else
+			date = Date.now() + dateStop - date1;
+		dateDisplay= new Date(date)
+		ms = dateDisplay.getMilliseconds();
+		sec = dateDisplay.getSeconds();
+		min = dateDisplay.getMinutes();
+		if (ms < 100)
+			ms = "00" + ms;
+		else if (ms < 10)
+			ms = "0" + ms;
+		if (sec < 10)
+			sec = "0" + sec;
+		if (min < 10)
+			min = "0" + min;
+		chrono = min+":"+sec;
+		var chronoP = document.getElementById("time")
+		chronoP.textContent = chrono
+	}, 50);
+
+}
+
+
+
+
+function stopChrono()
+{
+	clearTimeout(date2);
+	dateStop = date;
+}
+
+function resetChrono()
+{
+	date = 0;
+	clearTimeout(date2);
+	dateStop = 0;
+	date1 = 0;
+	var i = 0;
+	document.getElementById("chronotime").innerHTML = "00:00";
+	var t = document.getElementById("listasupprimer");
+	for(var liste in t){
+		t.parentElement.lastChild.remove();
+	}
+}
 
